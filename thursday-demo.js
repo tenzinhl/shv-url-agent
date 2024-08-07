75204e697a3cc02f6ce47cec49b2046375f0a5ac
Tenzin Low <tenzin@shv.com>
2024-08-07 14:24:55 -0700

    Thursday Demo file asks Claude about result relevance.

--- thursday-demo.js
+++ thursday-demo.js
@@ -20,17 +20,23 @@ puppeteer.use(StealthPlugin());
 import Anthropic from '@anthropic-ai/sdk';
 
 // Prompt for getting the top-level results.
-const GOOGLE_PROMPT = "Restaurants in my area"
+const GOOGLE_PROMPT = "Astera Labs Q2 Earnings 2024"
 // Prompt that's sent to Claude along with the image of the website to extract information from the site.
-const RESULT_PROMPT = `Your task is to evaluate the relevance of the provided webpage to the prompt: '${GOOGLE_PROMPT}'. \
-Your response must be one of the following: "HIGHLY RELEVANT", "SOMEWHAT RELEVANT", "SOMEWHAT IRRELEVANT", or "HIGHLY IRRELEVANT". \
-Do not include anything else in your response besides the provided response options.`;
+const RESULT_PROMPT = `Attached to this message is a screenshot of an article. \
+Your task is to evaluate the relevance of the attached article to the prompt: '${GOOGLE_PROMPT}'. \
+Your response should be a number from 1 to 5 with 1 being the least relevant and 5 being the most relevant. \
+Only respond with a number and nothing else.
+
+How relevant is the article to the prompt?`;
 // Number of Google results to show per page. Should match one of Google's allowed values: https://stackoverflow.com/a/30879675
 // Although some brief testing with small numbers like 1,2,3 suggest they work as well.
-const NUM_RESULTS_PER_PAGE = 3;
+const NUM_RESULTS_PER_PAGE = 100;
 
 // Total number of URLs to pull from Google.
-const TOTAL_RESULTS = 7;
+const TOTAL_RESULTS = 100;
+
+// Whether to run Puppeteer in Headless mode.
+const HEADLESS = false;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -38,7 +44,6 @@ const __dirname = dirname(__filename);
 console.log("__filename is: ", __filename);
 
 dotenv.config()
-
 
 // Function to log messages
 async function log(message) {
@@ -65,10 +70,6 @@ function makeUserMessage(input) {
         role: "user",
         content: [
             ...(input.prompt ? [{type: "text", text: input.prompt}] : []),
-            {
-                type: "text",
-                text: "List the top 3 most recommended restaurants in this image as a comma-separated list. Include ONLY the names of the restaurants and commas. If there are less than 3 restaurants, list all of them."
-            },
             ...(input.imageBase64 ? [{type: "image", source: {
                 type: "base64",
                 media_type: "image/png",
@@ -80,19 +81,25 @@ function makeUserMessage(input) {
 
 // Query Claude with the provided "input" object. Supports the following fields:
 async function queryClaude(input) {
-    return await axios.post('https://api.anthropic.com/v1/messages', {
+    var payload = {
         model: "claude-3-sonnet-20240229",
         max_tokens: 1024,
         messages: [
             makeUserMessage(input)
         ]
-        }, {
-        headers: {
+    };
+
+    // DEBUG: can remove.
+    // log(`Query Claude with message: ${JSON.stringify(payload)}`);
+
+    return await axios.post('https://api.anthropic.com/v1/messages',
+        payload,
+        { headers: {
             'Content-Type': 'application/json',
             'x-api-key': process.env['ANTHROPIC_API_KEY'],
             'anthropic-version': '2023-06-01'
-        }
-    });
+            }
+        });
 }
 
 // Sourced from https://www.useragents.me/
@@ -119,7 +126,7 @@ const client = new Anthropic({
   });
 
 const browser = await puppeteer.launch({
-    headless: false,
+    headless: HEADLESS,
 });
 const mainPage = await browser.newPage();
 await mainPage.setExtraHTTPHeaders(get_non_sus_headers());
@@ -140,8 +147,8 @@ while (resultCount < TOTAL_RESULTS) {
         pageResultIdx = 0;
 
         // Load a page of Google results.
-        await mainPage.goto(`https://www.google.com/search?q=${encodeURIComponent(GOOGLE_PROMPT)}&num=${NUM_RESULTS_PER_PAGE.toString()}&start=${(googlePage * NUM_RESULTS_PER_PAGE).toString()}`);
-        await mainPage.waitForNavigation();
+        await mainPage.goto(`https://www.google.com/search?q=${encodeURIComponent(GOOGLE_PROMPT)}&num=${NUM_RESULTS_PER_PAGE.toString()}&start=${(googlePage * NUM_RESULTS_PER_PAGE).toString()}`,
+            {waitUntil: ['networkidle2', 'domcontentloaded']});
 
         // Extract a list of URLs and Titles from the page of search results.
         // Each result is a JS object with a "title" and "url" property.
@@ -163,22 +170,24 @@ while (resultCount < TOTAL_RESULTS) {
             }
             return data;
         });
-    }
 
-    // If we've navigated to a new Google page and there's still no new results, abort.
-    if (pageResultIdx >= pageResultList.length) {
-        await log(`ERROR: No new results found on page ${googlePage}.`);
-        break;
+        // If we've navigated to a new Google page and there's still no new results, abort.
+        if (pageResultList.length == 0) {
+            await log(`ERROR: No new results found on page ${googlePage}.`);
+            break;
+        }
     }
 
     // Otherwise investigate next result.
     const item = pageResultList[pageResultIdx];
 
     // Open a new tab for the URL and navigate to it.
-    resultPage = await browser.newPage();
+    const resultPage = await browser.newPage();
     await resultPage.setExtraHTTPHeaders(get_non_sus_headers());
     try {
         await resultPage.goto(item.url, {waitUntil: ['networkidle2', 'domcontentloaded']});
+        // Hardcoded test
+        // await resultPage.goto("https://www.businesswire.com/news/home/20240806374053/en/Astera-Labs-Announces-Financial-Results-for-the-Second-Quarter-of-Fiscal-Year-2024", {waitUntil: ['networkidle2', 'domcontentloaded']});
     } catch (error) {
         await log(`ERROR: Failed to goto(${item.url}), message: ${error.message}`);
         await resultPage.close();
@@ -192,11 +201,14 @@ while (resultCount < TOTAL_RESULTS) {
             height: document.documentElement.scrollHeight,
         };
     })
+
+    // Seems like tuning this actually matters which is crazy. Running theory is that when the image is too long the model can forget about
+    // things that were earlier in the article.
     const screenshotBuffer = await resultPage.screenshot({ 
-        clip: { x: 0, y: 0, width: Math.min(resultDims.width, 8000), height: Math.min(resultDims.height, 8000) } });
+        clip: { x: 0, y: 0, width: Math.min(resultDims.width, 4000), height: Math.min(resultDims.height, 4000) } });
 
     // Save the screenshot temporarily
-    const tempFilePath = `imgs/temp_screenshot_${seq_num}.png`;
+    const tempFilePath = `imgs/temp_screenshot_${resultCount}.png`;
     await fs.writeFile(tempFilePath, screenshotBuffer);
 
     // Ask Claude about the page.
@@ -208,11 +220,17 @@ while (resultCount < TOTAL_RESULTS) {
             imageBase64: imageBase64
         });
     
-        // Log the response from Claude.
-        const claudeResponse = JSON.stringify(response.data.content);
-        await log(`Claude's response: ${claudeResponse}`);
-        item.relevance = claudeReponse;
-
+        // Log the response from Claude. We assume that first object in list is the right one.
+        const claudeResponse = response.data.content[0];
+        log(`claudeResponse is: ${JSON.stringify(claudeResponse)}`);
+        if (claudeResponse.type !== "text") {
+            await log(`ERROR: Claude's response was not text: ${JSON.stringify(response.data.content)}`);
+            item.relevance = "UNKNOWN";
+        } else {
+            item.relevance = claudeResponse.text;
+        }
+        await log(`Result: ${JSON.stringify(item)}`);
+        resultList.push(item);
     } catch (error) {
         await log(`ERROR calling Claude API: ${error.response ? JSON.stringify(error.response.data) : error.message}`);
     }
@@ -225,6 +243,9 @@ while (resultCount < TOTAL_RESULTS) {
     pageResultIdx++;
 }
 
-console.log(list);
+await log(`Final results: ${JSON.stringify(resultList)}`);
+
+// Now we can filter if we want.
+await log(`Highly relevant results: ${JSON.stringify(resultList.filter(item => item.relevance === "5"))}`);
 
 await browser.close();
